0e92c2acc8dce8d8ac7942dea0f5d9501d41ccee
fduquea <fduque-a@student.42porto.com>
2023-06-29 17:02:45 +0100

    4 problems fixed only 182 remain.

--- pipex.c
+++ pipex.c
@@ -6,34 +6,9 @@
 /*   By: fduque-a <fduque-a@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 13:05:29 by fduque-a          #+#    #+#             */
-/*   Updated: 2023/06/29 12:59:18 by fduque-a         ###   ########.fr       */
+/*   Updated: 2023/06/29 15:53:59 by fduque-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-/*
-Pipex
-
--I need to execute x number of commands with input from a file and I want to write the output to another file.
--I need to use dup and dup2 to change standard input and output fd from the terminal to the files.
--I need to create a pipe (maybe x amount of pipes?) and then fork (also x amound of times?)
--To exec the commands with execve I need to use the envp and access??
-
-Done -- I need to get the PATH variable from envp and remove the other ones. (split)
-
-Done -- Open infile to ready input and outfile to write output.
-
-Execve reads from STDIN and outputs to STDOUT 
-Make standard input the infile and make standard output to outfile (with dup/dup2)
-
-I already have a way to know the true path to use.
-
-I need to make the child process do the first command and the parent process do the second command.
-The child process reads from the infile and writes to the pipe.
-The parent process reads from the pipe and does the second command.
-The parent process writes to the outfile.
-I need to use waitpid to wait for the child process to finish before starting the parent process.
-
-*/
 
 #include "pipex.h"
 
@@ -62,6 +37,7 @@ char	*func_get_path(char *comm, char **envp)
 		free(path);
 		i++;
 	}
+	ft_printf("Add error message here.func_get_path.\n");
 	return (NULL);
 }
 
@@ -71,95 +47,36 @@ void	func_run_command(char *command, char **envp)
 	char	*path;
 	char	**args;
 
-	ft_printf("Line 74.\n");
 	args = ft_split(command, ' ');
 	path = func_get_path(args[0], envp);
 	execve(path, args, envp);
-}
-
-// In the first child process:
-// Close the unused end of the pipe.
-// Use dup2 to duplicate the read end of the pipe onto the standard input file descriptor (STDIN_FILENO).
-// Close the read end of the pipe.
-// Use open to open the output file and obtain the file descriptor for writing.
-// Use dup2 to duplicate the write end of the pipe onto the standard output file descriptor (STDOUT_FILENO).
-// Close the write end of the pipe.
-// Execute the second shell command using execve, passing the command and the necessary arguments.
-void func_child_process(int *pipefd, int infile, char **envp, char *argv, int i)
-{
-	if (i == 0)
-	{
-		ft_printf("Line 92.\n");
-		dup2(infile, STDIN_FILENO);
-		dup2(pipefd[1], STDOUT_FILENO);
-		close(pipefd[0]);
-		func_run_command(argv, envp);
-	}
-	else
-	{
-		ft_printf("Line 100.\n");
-		dup2(pipefd[0], STDIN_FILENO);
-		dup2(pipefd[1], STDOUT_FILENO);
-		func_run_command(argv, envp);
-	}
-	ft_printf("This should not print.Child.\n");
-}
-
-// In the second process:
-//         Close the write end of the pipe.
-//         Use dup2 to duplicate the read end of the pipe onto the standard input file descriptor (STDIN_FILENO).
-//         Close the read end of the pipe.
-//         Execute the first shell command using execve, passing the command and the necessary arguments.
-void func_parent_process(int *pipefd, int outfile, char **envp, char *argv)
-{
-	ft_printf("Line 115.\n");
-	dup2(outfile, STDOUT_FILENO);
-	dup2(pipefd[0], STDIN_FILENO);
-	func_run_command(argv, envp);
-	ft_printf("This should not print.Parent.\n");
+	ft_printf("Add error message here.func_run_command.\n");
 }
 
 // Create a pipe using the pipe function to establish communication between the parent process and the child process.
 // Use fork to create a child process.
-int	func_pipex(int infile, int outfile, char **envp, char **argv)
+void	func_pipex(int infile, char **envp, char *comm)
 {
-	int	pid;
-	int	pipefd[2];
-	int	i;
+	pid_t	pid;
+	int		pipefd[2];
 
-	i = 0;
-	if (pipe(pipefd) == -1)
-		return (1); 
-	while (argv[i + 3] != NULL)
+	pipe(pipefd);
+	pid = fork();
+	if (pid)
 	{
-		ft_printf("Line 136.\n");
-		pid = fork();
-		if (pid == -1)
-			return (1);
-		if (pid == 0)
-		{
-			func_child_process(pipefd, infile, envp, argv[i + 1], i);
-			exit(0);
-		}
-		waitpid(pid, NULL, 0);
-		i++;
-	}
-	close(pipefd[1]);
-	if (i == 0)
-	{
-		ft_printf("Line 147.\n");
-		dup2(outfile, STDOUT_FILENO);
-		dup2(infile, STDIN_FILENO);
 		close(pipefd[1]);
-		close(pipefd[0]);
-		func_run_command(argv[1], envp);
+		dup2(pipefd[0], STDIN_FILENO);
+		waitpid(pid, NULL, 0);
 	}
 	else
 	{
-		ft_printf("Line 156.\n");
-		func_parent_process(pipefd, outfile, envp, argv[i + 1]);
+		close(pipefd[0]);
+		dup2(pipefd[1], STDOUT_FILENO);
+		if (infile == STDIN_FILENO)
+			ft_printf("Add error message here.func_pipex.\n");
+		else
+			func_run_command(comm, envp);
 	}
-	return (0);
 }
 
 // make envp become a char** only containing the PATHS
@@ -184,6 +101,7 @@ char **func_get_real_envp(char **envp)
 		}
 		i++;
 	}
+	ft_printf("Add error message here.func_get_real_envp\n");
 	return (NULL);
 }
 
@@ -192,13 +110,25 @@ int	main(int argc, char **argv, char **envp)
 	int		infile;
 	int		outfile;
 	char	**real_envp;
+	int		i;
 
-	if (argc == 1)
-		return (1);
-	real_envp = func_get_real_envp(envp);
-	infile = open(argv[1], O_RDONLY);
-	outfile = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0777);
-	func_pipex(infile, outfile, real_envp, argv + 1);
-	free(real_envp);
-	return (0);
+	if (argc > 4)
+	{
+		i = 0;
+		real_envp = func_get_real_envp(envp);
+		infile = open(argv[1], O_RDONLY);
+		outfile = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+		dup2(infile, STDIN_FILENO);
+		dup2(outfile, STDOUT_FILENO);
+		func_run_command(argv[2], real_envp);
+		while (i + 3 < argc - 2)
+		{
+			func_pipex(infile, real_envp, argv[i + 3]);
+			i++;
+		}
+		func_run_command(argv[i + 3], real_envp);
+	}
+	else
+		ft_printf("Add error message here.main.\n");
+	return (1);
 }
